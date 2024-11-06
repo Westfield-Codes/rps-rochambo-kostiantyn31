2d48190234428433b323197050cd1ec60f3cdfb8
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-11-06 17:05:07 +0000

    cpuTurn

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function main() {
-    
+cpuTurn();
+
+function cpuTurn() {
+    let userChoice = Math.floor(Math.random()*3);
+    if (userChoice == 0) alert("Rock");
+    else if (userChoice == 1) alert("Paper");
+    else alert("Scissors");
+    return userChoice;
 }
