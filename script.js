bf8b9ac73342cb94e6cd7bf1806fc162c522053c
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-11-06 17:11:31 +0000

    cpuTurn

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-cpuTurn();
+main();
+
+function main(){
+    let uChoice = 0;
+    let cChoice = 0;
+    while (uChoice == cChoice){
+        uChoice = userTurn();
+        cChoice = CPUTurn();
+        if (uChoice == cChoice){
+            alert("We've both chosen the same move! We must now DANCE.");
+            alert("and by DANCE, I mean go again!");
+        }
+    }
+    findWinner(uChoice, cChoice);
+}
+
+function userTurn(move){
+    switch (move){
+        case 1:
+            alert("You've selected rock.");
+            return "rock";
+        case 2:
+            alert("You've selected paper.");
+            return "paper";
+        case 3:
+            alert("You've selected scissors.");
+            return "scissors";
+        default:
+            alert("It's your turn. Please, select a move!");
+            break;
+    }
+}
 
 function cpuTurn() {
-    let userChoice = Math.floor(Math.random()*3);
-    if (userChoice == 0) alert("Rock");
-    else if (userChoice == 1) alert("Paper");
+    let choice = Math.floor(Math.random()*3);
+    if (choice == 0) alert("Rock");
+    else if (choice == 1) alert("Paper");
     else alert("Scissors");
-    return userChoice;
+    return choice;
 }
