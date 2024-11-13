e2e59a9dcf28b371f7fed93280783237bd9a0ac3
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-11-13 13:43:22 +0000

    work

--- script.js
+++ script.js
@@ -1,65 +1,47 @@
-
-function main(){
+/* main */ 
+function main() {
     let uChoice = 0;
     let cChoice = 0;
-    while (uChoice == cChoice){
+    while (uChoice == cChoice ) {
         uChoice = userTurn();
         cChoice = cpuTurn();
-        if (uChoice == cChoice){
-            alert("We've both chosen the same move! We must now DANCE.");
-            alert("and by DANCE, I mean go again!");
-        }
+        if ( uChoice == cChoice ) alert("We both picked " + cChoice);
     }
-    findWinner(uChoice, cChoice);
+    findwinner(uChoice, cChoice);
 }
 
-function userTurn(move){
-    switch (move){
-        case 1:
-            alert("You've selected rock.");
-            return "r";
-        case 2:
-            alert("You've selected paper.");
-            return "p";
-        case 3:
-            alert("You've selected scissors.");
-            return "s";
-        default:
-            alert("It's your turn. Please, select a move!");
-            break;
+/* userTurn */ 
+function userTurn() {
+    let choice = prompt("Enter r, p or s")
+    if (choice != "r" && choice != "p" && choice != "s") {
+        alert("Must be r, p or s ")
+        return userTurn();
     }
-    
+    else return choice;
 }
 
+/* cpuTurn */ 
 function cpuTurn() {
-    let choice = Math.floor(Math.random()*3);
-    if (choice == 0) {
-        alert("Rock");
-        return "r";
-    } 
-    else if (choice == 1) {
-        alert("Paper");
-        return "p";
-    } 
-    else {
-        alert("Scissors");
-        return "s";
-    } 
+    let choice = Math.floor(Math.random()*2);
+    if (choice == 0) return "r"; 
+    else if (choice == 1) return "p";
+    else return "s";
 }
 
-function findWinner(uChoice, cChoice) {
-    let winner = "undefined";
-    if ( uChoice == "r" ) {
-        if ( cChoice == "s" ) winner = "player";
-        else winner = "cpu"; 
+/* findwinner */ 
+function findwinner(u, c) {
+    let winner = "";
+    if (u == "r") {
+        if ( c == "p") winner = "cpu";
+        else winner = "You";
     }
-    else if ( uChoice == "s" ) {
-        if ( cChoice == "p" ) winner = "player";
-        else winner = "cpu";
+    else if (u == "p") {
+        if ( c == "s") winner = "cpu";
+        else winner = "You";
     }
     else {
-        if ( cChoice == "r" ) winner = "player";
-        else winner = "cpu";
+        if ( c == "r") winner = "cpu";
+        else winner = "You";
     }
-    alert("You pick " + uChoice + " CPU pick " + cChoice + ". " + winner + " win");
+    alert("You pick " + u + " cpu pick " + c + ". " + winner + " win");
 }
