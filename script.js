99667e67c175d9614f4aede2a045b3c9d4916465
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-11-13 17:12:13 +0000

    ;(

--- script.js
+++ script.js
@@ -7,29 +7,47 @@ function main() {
         cChoice = cpuTurn();
         if ( uChoice == cChoice ) alert("We both picked " + cChoice);
     }
-    findwinner(uChoice, cChoice);
+    // findwinner(uChoice, cChoice);
 }
 
 /* userTurn */ 
-function userTurn() {
-    let choice = prompt("Enter r, p or s")
+/* function userTurn() {
+    let choice = prompt("Enter r, p or s");
     if (choice != "r" && choice != "p" && choice != "s") {
-        alert("Must be r, p or s ")
+        alert("Must be r, p or s ");
         return userTurn();
     }
     else return choice;
+} */
+
+function userTurn() {
+    let moves = ["r", "p", "s"];
+    let choice = prompt("Enter r, p or s");
+    if (moves.includes(choice)) {
+       return choice; 
+    }
+     else {
+        alert("Must be r, p or s ");
+        return choice;
+     }
 }
 
 /* cpuTurn */ 
-function cpuTurn() {
+/* function cpuTurn() {
     let choice = Math.floor(Math.random()*2);
     if (choice == 0) return "r"; 
     else if (choice == 1) return "p";
     else return "s";
+} */
+
+function cpuTurn() {
+    let moves = ["r", "p", "s"];
+    let choice = Math.floor(Math.random()*2);
+    return moves[choice];
 }
 
 /* findwinner */ 
-function findwinner(u, c) {
+/* function findwinner(u, c) {
     let winner = "";
     if (u == "r") {
         if ( c == "p") winner = "cpu";
@@ -44,4 +62,18 @@ function findwinner(u, c) {
         else winner = "You";
     }
     alert("You pick " + u + " cpu pick " + c + ". " + winner + " win");
-}
+} */
+
+function findwinner(u, c) {
+    turns = u+c;
+    let match = "";
+    let winArray=[
+        ["r","p","I"],["r","s","you"],
+        ["p","s","I"],["p","r","you"],
+        ["s","r","I"],["s","p","you"]];
+    for ( let = 0; i < winArray.length; i++ ) {
+        match = winArray[i][0] + winArray[i][1];
+        if (match == turns) winner = winArray[i][2];
+    }    
+        
+}  
